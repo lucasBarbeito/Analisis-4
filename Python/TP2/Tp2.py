0dce13ba8367d067f59e2af1f1e97d372c59e042
Lucas Barbeito <lucas.barbeito@ing.austral.edu.ar>
2020-11-09 19:49:58 -0300

    graficos listos

--- Python/TP2/Tp2.py
+++ Python/TP2/Tp2.py
@@ -1,4 +1,5 @@
 import math
+import matplotlib.pyplot as plt
 # z''' + b*z'' + c*z' + d*z = F(x)
 
 # Consideraremos a Z''' multiplicada por 1
@@ -26,11 +27,15 @@ def rk4(z, t, y, w, h):
     return z1
 
 
-def n_rk4(z, t, y, w, h, n):
+def n_rk4(z,y, w, numberOfIterations, initialValue, finalValue):
+    #FinalVaue es el valor de x que quiero estimar
+
+    t = initialValue
+    h = (finalValue - initialValue)/numberOfIterations
     z1 = [z, y, w]
     tArray = []
     zArray = []
-    for i in range(n):
+    for i in range(numberOfIterations):
         zArray.append(z1)
         z1 = rk4(z1[0], t, z1[1], z1[2], h)
         tArray.append(t)
@@ -52,17 +57,53 @@ def n_rk4(z, t, y, w, h, n):
     z5 = zArray[4][1] + h / 720 * (251 * f5prime + 646 * f4 - 264 * f3 + 106 * f2 - 19 * f1)
     w5 = zArray[4][2] + h / 720 * (251 * f5prime + 646 * f4 - 264 * f3 + 106 * f2 - 19 * f1)
 
-    print(y5)
-    print(z5)
-    print(w5)
+    #print(y5)
+   # print(z5)
+    #print(w5)
 
     value = function(t,w5,y5,z5)
-    print("value = ",value)
+    #print("value = ",value)
+    return value
+
+
+
+#n_rk4(1,-1,0,10,0,1)
+
+
+def plot(initialValue, finalValue, numberOfIterations):
+    plotList = [numberOfIterations , finalValue , n_rk4(1,-1,0,numberOfIterations,initialValue,finalValue)]
+   # print(plotList[1],plotList[2])
+    return plotList
+
+def showplot(numberOfIterations):
+    finalValues = []
+    yValues = []
+
+    for i in range (30) :
+        finalValues.append(i)
+
+    for i in range(30):
+        plot(0,finalValues[i],numberOfIterations)
+        yValues.append(plot(0,finalValues[i],numberOfIterations)[2])
+
+    print("for ",numberOfIterations," number of iterations")
+
+    for i in range(30):
+        print("for x value = ",finalValues[i], "f(x) = ", yValues[i])
+    plt.style.use('ggplot')
+    title = ('number of iterations = ', numberOfIterations)
+    plt.title(title)
+    plt.plot(finalValues,yValues)
+    plt.xlabel("x values")
+    plt.xlim(0,30)
+    plt.ylim(0,yValues[29])
+    plt.ylabel("y values")
+    plt.show()
+
+showplot(100)
+showplot(1000)
+showplot(10000)
 
 
 
 
-
-
-
-n_rk4(1, 0, -1, 0, 0.1, 5)
